ff34ea5599dae82a047d2d46036308f0b55c731f
Marian Ban <marian.ban@gmail.com>
2022-02-13 15:18:07 +0100

    new local db

--- DBClient.js
+++ DBClient.js
@@ -1,34 +1,56 @@
-import level from 'level';
+import path from 'path';
+import { JSONFile, Low } from 'lowdb';
 
-const db = level('publish-db');
+// import level from 'level';
+// const db = level('publish-db');
+
+const __dirname = path.resolve();
+const file = path.join(__dirname, 'publish-db.json');
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
 
 const getValue = async (key) => {
   try {
-    return await db.get(key);
+    await db.read();
+    db.data = db.data || {};
+    console.log(JSON.stringify(db.data));
+    return db.data[key];
   } catch (err) {
-    // console.error(err);
+    console.error(err);
+    return null;
+  }
+};
+
+const setValue = async (key, value) => {
+  try {
+    await db.read();
+    db.data = db.data || {};
+    db.data[key] = value;
+    db.write();
+  } catch (err) {
+    console.error(err);
     return null;
   }
 };
 
 export class DBClient {
   isProcessRunning = async () => {
-    if (!db.isOperational()) {
-      console.log('db is not operational');
-      return;
-    }
+    // if (!db.isOperational()) {
+    //   console.log('db is not operational');
+    //   return;
+    // }
     const value = await getValue('__process_running');
-    const isRunning = value === 'true';
+    const isRunning = !!value;
     console.log(`isRunning: ${isRunning}`);
     return isRunning;
   };
   setProcessAsRunning = async () => {
     console.log('setting process as running');
-    await db.put('__process_running', true, { sync: true });
+    await setValue('__process_running', true);
   };
   setProcessAsFinished = async () => {
     console.log('setting process as finished');
-    await db.put('__process_running', false, { sync: true });
+    await setValue('__process_running', false);
   };
   getLastBuildDate = async (dir) => {
     const value = await getValue(`__last_build_date__${dir}`);
@@ -37,8 +59,6 @@ export class DBClient {
   };
   setLastBuildDate = async (dir, date) => {
     console.log(`setting last build date to ${date} for ${dir}`);
-    await db.put(`__last_build_date__${dir}`, new Date(date).toISOString(), {
-      sync: true,
-    });
+    await setValue(`__last_build_date__${dir}`, new Date(date).toISOString());
   };
 }
